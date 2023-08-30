4176686b815a3465a4d293d5ffa4aa70050c0831
Tuvshinjargal Bayandorj <tuvshinjargal01@gmail.com>
2023-08-30 13:35:30 +0800

    courses page

--- app/courses/page.js
+++ app/courses/page.js
@@ -6,13 +6,17 @@ function Courses() {
     <div className="w-full ">
       <div className="grid grid-cols-4 gap-4">
         <div className="col-span-1 bg-white border rounded-md overflow-hidden ">
-          <div className="flex items-center justify-between">
+          <div className="flex flex-col items-center justify-between">
             <div className="relative aspect-video w-full">
               <Image
                 src="https://picsum.photos/300/200"
                 fill
                 style={{ objectFit: "cover" }}
               />
+              <div className="flex flex-col">
+                <div className="">Name</div>
+                <div className="">others</div>
+              </div>
             </div>
           </div>
         </div>
