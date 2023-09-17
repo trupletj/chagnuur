4ece1e52d30d30ca7179e2b23d3e7827e5eb8f35
unknown <enkhjin1992@gmail.com>
2023-09-18 01:36:55 +0800

    ee

--- components/Test.js
+++ components/Test.js
@@ -5,7 +5,7 @@ import useSWR from "swr";
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 function Test() {
   const { data, error, isLoading } = useSWR(
-    "http://localhost:3000/api/courses",
+    "http://admin.chagnuur.mn/api/client/model/list",
     fetcher
   );
   if (error) return <div> "An error has occurred."</div>;
