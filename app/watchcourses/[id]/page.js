e40d98f00b783e729ac108177a075cc0ae947e0a
Tuvshinjargal Bayandorj <tuvshinjargal01@gmail.com>
2023-09-17 21:59:03 +0800

    ss

--- app/watchcourses/[id]/page.js
+++ app/watchcourses/[id]/page.js
@@ -6,17 +6,10 @@ function WatchCourses() {
   return (
     <div className="flex flex-row w-full overflow-hidden">
       <div className="w-full h-screen bg-red-300 relative">
-        <content>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum
-        </content>
+        <div className="m-10 flex">
+          {/* Content here */}
+          // video content here
+        </div>
       </div>
       <div
         className={classNames({
